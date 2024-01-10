f08d9c04585d6eb7fedcc697c5d90ebf3e8b8734
amaftah <abderrahmanemaftah@outlook.com>
2024-01-10 11:10:28 +0000

    t

---
+++ printcomb2.go
@@ -0,0 +1,27 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb2() {
+	for a := '0'; a <= '9'; a++ {
+		for b := '0'; b <= '9'; b++ {
+			c := a
+			d := b + 1
+			for ; c <= '9'; c++ {
+				for ; d <= '9'; d++ {
+					z01.PrintRune(a)
+					z01.PrintRune(b)
+					z01.PrintRune(' ')
+					z01.PrintRune(c)
+					z01.PrintRune(d)
+					if a != '9' || b != '8' || c != '9' || d != '9' {
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
+				}
+				d = '0'
+			}
+		}
+	}
+	z01.PrintRune('\n')
+}
