464e4580401119c43f678a948dca453c996fc0c0
Abderrahmane Maftah <78481502+amaftah@users.noreply.github.com>
2024-01-13 16:14:47 +0000

    t

---
+++ QuadA.go
@@ -0,0 +1,30 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main() {
+	QuadA(10, 10)
+}
+
+func QuadA(x, y int) {
+	if x > 0 || y > 0 {
+		for i := 0; i < y; i++ {
+			for j := 0; j < x; j++ {
+				switch {
+				case (i == 0 || i == y-1) && (j == 0 || j == x-1):
+					z01.PrintRune('o')
+				case i == 0 || i == y-1:
+					z01.PrintRune('-')
+				case j == 0 || j == x-1:
+					z01.PrintRune('|')
+				default:
+					z01.PrintRune(' ')
+				}
+			}
+			z01.PrintRune('\n')
+		}
+	} else {
+		return
+	}
+
+}
