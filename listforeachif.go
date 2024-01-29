3ccac244fc60696dc36724de5075b3018a5bed8c
amaftah <abderrahmanamaftah@outlook.com>
2024-01-29 10:49:16 +0100

    t

--- listforeachif.go
+++ listforeachif.go
@@ -1,4 +1,6 @@
-package piscine
+package main
+
+import "fmt"
 
 type NodeL struct {
 	Data interface{}
@@ -36,4 +38,39 @@ func ListForEachIf(l *List, f func(*NodeL), cond func(*NodeL) bool) {
 		}
 		current = current.Next
 	}
+}
+
+func PrintList(l *piscine.NodeI) {
+	it := l
+	for it != nil {
+		fmt.Print(it.Data, " -> ")
+		it = it.Next
+	}
+	fmt.Print(nil, "\n")
+}
+
+func listPushBack(l *piscine.NodeI, data int) *piscine.NodeI {
+	n := &piscine.NodeI{Data: data}
+
+	if l == nil {
+		return n
+	}
+	iterator := l
+	for iterator.Next != nil {
+		iterator = iterator.Next
+	}
+	iterator.Next = n
+	return l
+}
+
+func main() {
+	var link *NodeI
+
+	link = listPushBack(link, 5)
+	link = listPushBack(link, 4)
+	link = listPushBack(link, 3)
+	link = listPushBack(link, 2)
+	link = listPushBack(link, 1)
+
+	PrintList(ListSort(link))
 }
