163b404bd1bbf60c3cfd042d418811a2755f3efe
amaftah <abderrahmanamaftah@outlook.com>
2024-01-30 11:31:29 +0100

    t

---
+++ btreemin.go
@@ -0,0 +1,35 @@
+package piscine
+
+type TreeNode struct {
+	Left, Right, Parent *TreeNode
+	Data                string
+}
+
+func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+	if root == nil {
+		return &TreeNode{Data: data}
+	}
+
+	if data < root.Data {
+		root.Left = BTreeInsertData(root.Left, data)
+		root.Left.Parent = root
+	} else if data > root.Data {
+		root.Right = BTreeInsertData(root.Right, data)
+		root.Right.Parent = root
+	}
+
+	return root
+}
+
+func BTreeMin(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	node := root
+	for node.Left != nil {
+		node = node.Left
+	}
+
+	return node
+}
