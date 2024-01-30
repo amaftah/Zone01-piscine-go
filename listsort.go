3cf0b1da889399120913ee797a5bee7fc8ae497a
amaftah <abderrahmanamaftah@outlook.com>
2024-01-30 11:07:02 +0100

    t

---
+++ listsort.go
@@ -0,0 +1,32 @@
+package piscine
+
+type NodeI struct {
+	Data int
+	Next *NodeI
+}
+
+type List struct {
+	Head *NodeI
+	Tail *NodeI
+}
+
+func ListSort(l *NodeI) *NodeI {
+	if l == nil || l.Next == nil {
+		return l
+	}
+
+	swapped := true
+	for swapped {
+		swapped = false
+		current := l
+		for current.Next != nil {
+			if current.Data > current.Next.Data {
+				current.Data, current.Next.Data = current.Next.Data, current.Data
+				swapped = true
+			}
+			current = current.Next
+		}
+	}
+
+	return l
+}
